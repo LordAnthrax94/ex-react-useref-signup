aa7a24cd16c79d8d3e4f6734664c1f8d00a463a9
LordAnthrax94 <andreagi1994@gmail.com>
2025-05-30 07:37:25 +0200

    Fix: completati input e preparazione per la validation

--- src/App.jsx
+++ src/App.jsx
@@ -1,5 +1,4 @@
-import { useState, useEffect, useRef } from 'react''
-import './App.css'
+import { useState, useEffect, useRef } from 'react'
 
 function App() {
   
@@ -13,6 +12,8 @@ function App() {
   const handlerSubmit = (e) => {
     e.preventDefault()
   }
+
+  const validationForm = 
     
   
 
@@ -21,27 +22,62 @@ function App() {
       <h1>Web-developer Sign-up</h1>
         <form onSubmit={handlerSubmit}>
           <label>
-            <input 
-              type="text"
-              value={name}
-              onChange={(e) => setName(e.target.value)}
-              placeholder="Name"
-            />
-            <input 
-              type="text"
-              value={username} 
-              onChange={(e) => setUserName(e.target.value)}
-              placeholder='Username'
-            />
-            <input 
-              type="password"
-              value={password}
-              onChange={(e) => setPassword(e.target.value)}
-              placeholder='Password' 
-            />
-            <select name="" id=""></select>
+              <p>Nome Completo</p>
+                <input 
+                  type="text"
+                  value={name}
+                  onChange={(e) => setName(e.target.value)}
+                  placeholder="Inserisci nome completo"
+                />
+            </label>
+            <label>
+              <p>Scegli il tuo username</p>
+                <input 
+                  type="text"
+                  value={username} 
+                  onChange={(e) => setUserName(e.target.value)}
+                  placeholder='Inserisci un username'
+                />
+            </label>
+            <label>
+              <p>Scegli una password</p>
+                <input 
+                  type="password"
+                  value={password}
+                  onChange={(e) => setPassword(e.target.value)}
+                  placeholder='Inserire password' 
+                />
+            </label>
+            <label>
+                <p>Seleziona Specializzazione</p>
+                  <select              
+                    value={spec}
+                    onChange={(e) => setSpec(e.target.value)} 
+                  >
+                    <option value="">-- Seleziona --</option>
+                    <option value="frontend">Frontend</option>
+                    <option value="backend">Backend</option>
+                    <option value="fullstack">Fullstack</option>
+                  </select>
+            </label>
+            <label>
+              <p>Inserisci Anni di Esperienza</p>
+                <input 
+                  type="number"
+                  value={years}
+                  onChange={(e) => setYears(e.target.value)}
+                  placeholder='Inserisci anni di esperienza'
+                />
+            </label>
+            <label>
+              <p>Inserisci la tua descrizione</p>
+                <textarea 
+                  value={description} 
+                  onChange={e => setDescription(e.target.value)} 
+                /> 
+            </label>           
 
-          </label>
+          
         </form>
         
       
